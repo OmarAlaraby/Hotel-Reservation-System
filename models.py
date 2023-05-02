d66207259de5dde20cb1255b361ad461b3b3120f
Helmy-JR <boda.badoda27@gmail.com>
2023-05-02 13:44:36 +0300

    adding models

---
+++ models.py
@@ -0,0 +1,22 @@
+class Room :
+    def __init__(self, room_id, capicity,status,room_type,rent):
+        self.room_id = room_id
+        self.capicity = capicity
+        self.status = status
+        self.room_type = room_type
+        self.rent = rent
+        
+class Customer :
+    def __init__(self,id, fullname,age,address,phonenumber,checkin_date,checkout_date,nationality,payment,object_room,national_id,email,reservation_status):
+        self.id = id
+        self.fullname = fullname
+        self.age = age
+        self.address = address
+        self.checkin_date = checkin_date
+        self.checkout_date = checkout_date
+        self.nationality = nationality
+        self.payment = payment
+        self.object_room = object_room
+        self.national_id = national_id
+        self.email = email
+        self.reservation_status = reservation_status
